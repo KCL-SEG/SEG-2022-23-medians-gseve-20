3cdfd2971da4bf7bdeefae2627ea9ad0bb9f9be4
Gabriela Sevelkova <gsevelkova5@gmail.com>
2023-10-01 19:27:20 +0100

    Adding solution 1

--- median.py
+++ median.py
@@ -9,4 +9,15 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+numbers.sort()
+if len(numbers)%2 == 1:
+    index = len(numbers)//2
+    answer = numbers[index]
+else:
+    indexOne = len(numbers)//2 - 1
+    indexTwo = len(numbers)//2
+    answer = (numbers[indexOne] + numbers[indexTwo])/2
+print(answer)
+
+
+
